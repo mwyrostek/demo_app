368d6ba2059beba4817c10add4c71e7e7708f9df
Michał Wyrostek <wyrostekmichal@gmail.com>
2012-11-21 11:14:51 +0100

    przyklad komitu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '67553aef7f2fa65caa61a3394d55a6ec156014619740e7662d84313a6a9118efd28a71b5c767dc53231d941bffd09452649fb58349a2eab80b1f47921bb0bffb'
